32507db76048bb83ea31042ce8775dfffe0aa27c
DeviSree-21 <devisreenarayan@gmail.com>
2026-08-04 00:09:10 +0530

    Create sequences for product and customer keys
    Added sequences for product and customer keys.

---
+++ SQL Scripts/02_Create_Sequence.sql
@@ -0,0 +1,8 @@
+-- ==========================================================
+-- Project : Retail Sales Data Warehouse
+-- File    : 02_Create_Sequence.sql
+-- ==========================================================
+
+CREATE OR REPLACE SEQUENCE PRODUCT_KEY_SEQ START = 1 INCREMENT = 1;
+
+CREATE OR REPLACE SEQUENCE CUSTOMER_KEY_SEQ START = 1 INCREMENT = 1;
